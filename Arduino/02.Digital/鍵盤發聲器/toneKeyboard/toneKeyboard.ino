323806a03a371228c84aff708d3f94b54e6c0320
Iphone99s-Plus <gn01868184@gmail.com>
2016-01-26 17:51:15 +0800

    Update toneKeyboard.ino

--- Arduino/02.Digital/鍵盤發聲器/toneKeyboard/toneKeyboard.ino
+++ Arduino/02.Digital/鍵盤發聲器/toneKeyboard/toneKeyboard.ino
@@ -16,13 +16,13 @@ This example code is in the public domain.
 
  http://www.arduino.cc/en/Tutorial/Tone3
 
- */
+*/
 
 #include "pitches.h"
 
-const int threshold = 10;     // 生成一個數為感測器的最小讀數
+const int threshold = 10;                                        // 生成一個數為感測器的最小讀數
 
-int notes[] = {               // notes 相當於三個感測器
+int notes[] = {                                                  // notes 相當於三個感測器
   NOTE_A4, NOTE_B4, NOTE_C3
 };
 
@@ -31,9 +31,9 @@ void setup() {
 
 void loop() {
   for (int thisSensor = 0; thisSensor < 3; thisSensor++) {
-    int sensorReading = analogRead(thisSensor);     // 得到一個感測器的讀數
-    if (sensorReading > threshold) {                // 如果感測器讀到的值夠大
-      tone(8, notes[thisSensor], 20);                 // 播放其屬的音調
+    int sensorReading = analogRead(thisSensor);                 // 得到一個感測器的讀數
+    if (sensorReading > threshold) {                            // 如果感測器讀到的值夠大
+      tone(8, notes[thisSensor], 20);                           // 播放其屬的音調
     }
   }
 }
