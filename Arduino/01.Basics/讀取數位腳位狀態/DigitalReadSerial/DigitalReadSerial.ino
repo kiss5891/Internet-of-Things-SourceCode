0d725cb99a0581190d328cf380788702b8c8b64c
Iphone99s-Plus <gn01868184@gmail.com>
2016-01-26 17:36:02 +0800

    Update DigitalReadSerial.ino

--- Arduino/01.Basics/讀取數位腳位狀態/DigitalReadSerial/DigitalReadSerial.ino
+++ Arduino/01.Basics/讀取數位腳位狀態/DigitalReadSerial/DigitalReadSerial.ino
@@ -5,16 +5,16 @@
  這個範例程式碼是在公共區域.
  */
 
-int pushButton = 2;       // 數字腳位 2 連接到一個按鈕. 給他一個名字
-void setup() {            // 當按下 reset 設置例行程式並運行一次
+int pushButton = 2;                           // 數字腳位 2 連接到一個按鈕. 給他一個名字
+void setup() {                                // 當按下 reset 設置例行程式並運行一次
   Serial.begin(9600);                         // 初始化序列通訊為 9600 bps
-  pinMode(pushButton, INPUT);                // 設定按鈕為輸入
+  pinMode(pushButton, INPUT);                 // 設定按鈕為輸入
 }
 
-void loop() {             // 這個循環回圈會不斷的重複
-  int buttonState = digitalRead(pushButton); // 讀取輸入腳位
+void loop() {                                 // 這個循環回圈會不斷的重複
+  int buttonState = digitalRead(pushButton);  // 讀取輸入腳位
   Serial.println(buttonState);                // 印出按鈕狀態
-  delay(1);                                  // 延遲讀取來維持穩定
+  delay(1);                                   // 延遲讀取來維持穩定
 }
 
 
