1b0be6c62779f7510ccecfcb7489525e832049b9
Iphone99s-Plus <gn01868184@gmail.com>
2016-01-27 10:36:39 +0800

    Update AnalogInOutSerial.ino

--- Arduino/03.Analog/手動亮度調整/AnalogInOutSerial/AnalogInOutSerial.ino
+++ Arduino/03.Analog/手動亮度調整/AnalogInOutSerial/AnalogInOutSerial.ino
@@ -16,24 +16,24 @@
 
  This example code is in the public domain.
 
- */
+*/
 
-const int analogInPin = A0;   // 可變電阻連接類比輸出引腳
-const int analogOutPin = 9;   // LED 連接類比輸出引腳
+const int analogInPin = A0;                             // 可變電阻連接類比輸出引腳
+const int analogOutPin = 9;                             // LED 連接類比輸出引腳
 
-int sensorValue = 0;          // 從 pot 讀取的值
-int outputValue = 0;          // 從 PWM 輸出的值 (類比輸出)
+int sensorValue = 0;                                    // 從 pot 讀取的值
+int outputValue = 0;                                    // 從 PWM 輸出的值 (類比輸出)
 
 void setup() {
-  Serial.begin(9600);                                  // 初始化序列通訊為 9600 bps
+  Serial.begin(9600);                                   // 初始化序列通訊為 9600 bps
 }
 
 void loop() {
-  sensorValue = analogRead(analogInPin);              // 讀取類比的值
-  outputValue = map(sensorValue, 0, 1023, 0, 255);    // map 類比輸出的範圍
-  analogWrite(analogOutPin, outputValue);             // 改變類比輸出的值
+  sensorValue = analogRead(analogInPin);                // 讀取類比的值
+  outputValue = map(sensorValue, 0, 1023, 0, 255);      // map 類比輸出的範圍
+  analogWrite(analogOutPin, outputValue);               // 改變類比輸出的值
   
-  Serial.print("sensor = ");                           // 列印結果到 serial monitor
+  Serial.print("sensor = ");                            // 列印結果到 serial monitor
   Serial.print(sensorValue);
   Serial.print("\t output = ");
   Serial.println(outputValue);
