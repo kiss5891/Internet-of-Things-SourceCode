818c7a41a8d0f20ace0696ba58a26ac77678e764
Danny Ho <kiss5891@gmail.com>
2016-02-13 19:25:36 +0800

    Update Publish.js

--- MQTT/Publish.js
+++ MQTT/Publish.js
@@ -1,9 +1,7 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://192.168.1.18');
-var a=0
 client.on('connect', function() {
   setInterval(function() {
-    client.publish('you_topic', a);
-    a++;
+    client.publish('you_topic', 'demo');
   }, 3000);
 });
